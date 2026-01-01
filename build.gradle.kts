6f01087f7b5d1b7be7c8039779c5289eefdc73a4
AvitoFeedPro <avitofeedpro@example.com>
2026-01-01 11:45:11 +0300

    Fix Compose compiler plugin for Kotlin 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,5 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("com.android.library") version "8.5.2" apply false
     kotlin("android") version "2.0.21" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
