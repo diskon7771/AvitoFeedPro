c07cab8760fd37e06d86cb44426e1b7904761f1d
AvitoFeedPro <avitofeedpro@example.com>
2026-01-01 15:31:16 +0300

    Fix Kotlin/Compose compatibility (Kotlin 1.9.22)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,24 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("org.jetbrains.kotlin.android") version "1.9.24"
-        id("org.jetbrains.kotlin.jvm") version "1.9.24"
-        id("org.jetbrains.kotlin.kapt") version "1.9.24"
-        id("com.google.dagger.hilt.android") version "2.51"
+        id("org.jetbrains.kotlin.android") version "1.9.22"
+        id("org.jetbrains.kotlin.jvm") version "1.9.22"
+        id("org.jetbrains.kotlin.kapt") version "1.9.22"
     }
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "AvitoFeedPro"
-
-include(":app")
-include(":domain")
-include(":data")
-include(":ui")
