9516892e27d80476d94b72455e913771e1a47a83
AvitoFeedPro <avitofeedpro@example.com>
2026-01-01 14:30:45 +0300

    Fix Kotlin version for Compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("com.android.library") version "8.5.2" apply false
-    kotlin("android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    kotlin("android") version "1.9.24" apply false
+    kotlin("kapt") version "1.9.24" apply false
 }
