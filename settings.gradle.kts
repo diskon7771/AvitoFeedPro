3f34ae4d75a25d648aec5703d4514231dc0a061a
AvitoFeedPro <avitofeedpro@example.com>
2026-01-01 14:06:25 +0300

    Fix Hilt plugin setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,16 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.google.dagger.hilt.android" -> {
+                    useModule("com.google.dagger:hilt-android-gradle-plugin:2.51")
+                }
+            }
+        }
+    }
 }
 
 dependencyResolutionManagement {
